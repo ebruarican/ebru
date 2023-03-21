603082298b59f96a8b20b9da3c54a5764dc83699
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-03-20 20:11:24 -0400

    pr3_2.js

---
+++ prjs3_2.js
@@ -0,0 +1,16 @@
+var myheading = "<h1>This is my web page!</h1>"
+var linktag = "<a href=\”http://www.webxpertz.net\”>WebSiteLink!</a>"
+var sometext="This text can be affected by other statements."
+var begineffect= "<em>"
+var endeffect= "</em>"
+var beginpara="<p>"
+var endpara="</p>";
+document.write(myheading);
+document.write(begineffect+sometext+endeffect);
+document.write(beginpara+linktag+endpara);
+document.write(beginpara+sometext+endpara);
+
+
+
+
+
