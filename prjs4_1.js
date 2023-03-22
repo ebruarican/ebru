eb567c8288ff0099454ff10f4954ffbd65c01f45
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-03-22 17:43:59 -0400

    prjs4_1.js

--- prjs4_1.js
+++ prjs4_1.js
@@ -1,11 +1,12 @@
 function car_cost(mycar,paycheck){
-    window.alert("You have a"+mycar+"and make $"+paycheck);
+    window.alert("You have a" +mycar+ "and make $"+paycheck);
 }
 function get_added_text(){
     var text1="This project";
-    var text2="is almost fun!";
+    var text2=" is almost fun!";
     var addedtext=text1+text2;
     return addedtext
 }    
 car_cost("Mustang",1500);
 var alerttext=get_added_text();
+window.alert(alerttext)
