d6373bfac8c789eb4d21671f4d250119952df76e
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-03-23 19:03:58 -0400

    march22.html

--- prjs4_2.js
+++ prjs4_2.js
@@ -1,6 +1,6 @@
 function display_HTML(){
     let myheading="<h1>Hello,World!</h1>";
-    let mytext="<p>While it is nice to know you world, there are only some things that I am comfortable sharing in a global context. You can’t alter the variable that holds this text outside of the block that contains it! Ha!</p>";
+    let  mytext="<p>While it is nice to know you world, there are only some things that I am comfortable sharing in a global context. You can’t alter the variable that holds this text outside of the block that contains it! Ha!</p>";
     document.write(myheading+mytext);
 }
 display_HTML();
