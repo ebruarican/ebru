48b31a2d477daf2ed7f37fa53af6aa902069b749
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-03-31 14:31:13 -0400

    prjs6_2.js

---
+++ prjs6_2.js
@@ -0,0 +1,21 @@
+let msg = "",
+    city= window.prompt('What is the name of  your city?', "");
+if ( ( city==="")||(city===null )){
+    msg ="no city entered";}
+else{   
+switch (city){
+    case "Jhonstown":
+        msg = "We can have items delivered to you in 3 days.";
+        break;
+    case 'Donville':
+        msg = "We can have items delivered to you in 1 week.";
+        break;
+    case "Danieltown || Martyville" :
+        msg = "We can have items delivered to you in 2 weeks.";
+        break;
+        default:
+        msg ="Sorry! We do not deliver to your city yet";
+           
+}
+}
+document.write(msg);
