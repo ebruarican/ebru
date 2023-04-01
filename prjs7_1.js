38721a656227a9dc8e82d1d9db54ae7f309a8647
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-04-01 09:30:08 -0400

    prjs7_1.js

--- prjs7_1.js
+++ prjs7_1.js
@@ -1,5 +1,5 @@
 let computer_parts = ["Monitors", "Motherboards", "Chips", "Hard Drives", "DVD-ROMs", "Cases", "Power", "Supplies"];
 document.write(<p>Computer parts:</p>)
-for (let p=0; p<computer_parts.length; p++ ){
-    document.write(computer_parts[p]+"<br>");
+for (let i=0; i<computer_parts.length; i++ ){
+    document.write(computer_parts[i]+"<br>");
 }
